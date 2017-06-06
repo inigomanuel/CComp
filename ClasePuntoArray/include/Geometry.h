fb8012beca06901681baff7d0831fe696fbefa1c
Inigo <i.diezcansf@hotmail.com>
2017-06-05 22:55:51 -0500

    Add files via upload

---
+++ ClasePuntoArray/include/Geometry.h
@@ -0,0 +1,16 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+
+class Geometry
+{
+    public:
+        Geometry();
+        virtual ~Geometry();
+
+    protected:
+
+    private:
+};
+
+#endif // GEOMETRY_H
